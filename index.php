1561ca6a98efe21e796ec2c5ddee2dd86276259b
cyeongb <cyeongb@gmail.com>
2022-05-24 00:52:53 +0900

    create php

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php echo 'Hello1234'; ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <title>Document</title>
+</head>
+
+<body>
+    <h2> <?php echo 'html document'; ?></h2>
+
+</body>
+
+</html>
